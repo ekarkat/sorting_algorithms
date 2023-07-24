49459765fbb3f6e392e4b80e70dddf0b89b2ec71
ekarkat <ekarkat@gmail.com>
2023-07-24 04:52:37 +0100

    fix sort.h

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorting list
+ * @list : a list the head of it
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = *list;
+	
+
+}
