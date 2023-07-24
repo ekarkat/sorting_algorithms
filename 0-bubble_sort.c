c7aff77f2a79a6585dc7c2ca518865fdaa65545d
ekarkat <ekarkat@gmail.com>
2023-07-24 04:48:41 +0100

    fix 0-bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,32 @@
 #include "sort.h"
+
 /**
  * bubble_sort - sort an array using bubble alghortihm
  * @array: the array
  * @size : the size
 */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t tmp;
 	size_t j;
 
+	if (array == NULL || size == 0 || size == 1)
+		return;
+
 	for (j = 0; j < size; j++)
 	{
 		i = 0;
-		for (;i < size - 1; i++)
+		for (; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				tmp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = tmp;
+				print_array(array, size);
 			}
 		}
-		print_array(array, size);
 	}
 }
