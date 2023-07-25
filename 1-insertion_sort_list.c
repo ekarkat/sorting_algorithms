19fd7f4d77f06837fcfc2a75589e579f3712df0f
ekarkat <ekarkat@gmail.com>
2023-07-25 22:33:19 +0100

    coka

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,23 @@
 #include "sort.h"
 
 /**
+ * count - count
+ * @list : list
+ * Return: return number
+*/
+int count(listint_t *list)
+{
+	int i = 0;
+
+	while (list != NULL)
+	{
+		list = list->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
  * switchi - switch two nodes in a list
  * @node1 : node 1
  * @node2 : node 2
@@ -11,6 +28,15 @@ void switchi(listint_t *node1, listint_t *node2, listint_t **list)
 {
 	if (node1->prev == NULL)
 	{
+		if (count(*list) == 2)
+		{
+			node1->next = NULL;
+			node2->next = node1;
+			node1->prev = node2;
+			node2->prev = NULL;
+			*list = node2;
+			return;
+		}
 		node2->next->prev = node1;
 		node2->prev = NULL;
 		node1->next = node2->next;
@@ -53,8 +79,12 @@ void insertion_sort_list(listint_t **list)
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	current = *list;
-	next = current->next;
-
+	if (count(current) == 2)
+	{
+		if (current->n > current->next->n)
+			switchi(current, current->next, list);
+		return;
+	}
 	while (current != NULL)
 	{
 		if (current->next == NULL)
@@ -67,7 +97,7 @@ void insertion_sort_list(listint_t **list)
 			print_list(*list);
 			rec = next;
 			prev = next->prev;
-			while ((rec->n < prev->n))
+			while (prev != NULL && (rec->n < prev->n))
 			{
 				switchi(prev, rec, list);
 				print_list(*list);
